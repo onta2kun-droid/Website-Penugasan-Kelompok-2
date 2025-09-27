003366bfc2e4243d0714db42c2a10bd664cfc465
onta2kun-droid <onta2kun@gmail.com>
2025-09-27 21:28:26 +0800

    Versi 5 (Mobile Update V5)

--- produk.js
+++ produk.js
@@ -83,10 +83,3 @@ produkCards.forEach(card => {
 
   updateTotal();
 });
-
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-});
